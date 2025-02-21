f61dc7b1ab0e554eca3e8cf3534d8abe731428bc
SeydaBeypazarli <seydabeypazarli@gmail.com>
2025-02-21 18:15:07 +0300

    s14 d1 proje tamamlandı

--- src/main/java/com/workintech/cylinder/Circle.java
+++ src/main/java/com/workintech/cylinder/Circle.java
@@ -4,13 +4,13 @@ public class Circle {
     private double radius;
 
     public Circle(double radius) {
-        this.radius = radius < 0 ? 0: radius;
+        this.radius = radius <0 ? 0 : radius;;
     }
 
     public double getRadius() {
         return radius;
     }
     public double getArea(){
-      return Math.PI * Math.pow(radius, 2);
+        return Math.PI * Math.pow(radius, 2);
     }
 }
--- src/main/java/com/workintech/pool/Cuboid.java
+++ src/main/java/com/workintech/pool/Cuboid.java
@@ -3,23 +3,20 @@ package com.workintech.pool;
 public class Cuboid extends Rectangle {
     private double height;
 
-    // Constructor
     public Cuboid(double width, double length, double height) {
         super(width, length); // Call the constructor of Rectangle
         setHeight(height);
     }
 
-    // Setter for height
+
     public void setHeight(double height) {
         this.height = (height < 0) ? 0 : height;
     }
 
-    // Getter for height
     public double getHeight() {
         return height;
     }
 
-    // Method to calculate volume
     public double getVolume() {
         return getArea() * height; // Area of the base * height
     }
